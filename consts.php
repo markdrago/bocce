3eb77f1fc321647ea603af5f6ba8b4ff615d5186
mdrago <>
2005-07-02 13:07:22 -0400

    [svn] - moved vardefs to consts and added STAT_* globals

---
+++ consts.php
@@ -0,0 +1,13 @@
+<?
+
+#definitions used in statslib.php
+define('STAT_GLOBAL', 0);
+define('STAT_LEAGUE', 1);
+define('STAT_SEASON', 2);
+
+$config_file = "config.php";
+require($config_file);
+
+$schema_file = "schema/$dbengine.sql";
+
+?>
--- vardefs.php
+++
@@ -1,8 +0,0 @@
-<?
-
-$config_file = "config.php";
-$schema_file = "schema/$dbengine.sql";
-
-require($config_file);
-
-?>
