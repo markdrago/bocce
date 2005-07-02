97fbffba1bc7214e59bdeefe2c9ae0b274a06c54
jeffpc <>
2005-07-02 10:34:23 -0400

    [svn] Added sample ball colors

---
+++ schema/sample-ball.sql
@@ -0,0 +1,8 @@
+INSERT INTO ball (num, color) VALUES (1,'Red');
+INSERT INTO ball (num, color) VALUES (1,'Blue');
+INSERT INTO ball (num, color) VALUES (1,'Yellow');
+INSERT INTO ball (num, color) VALUES (1,'Red / Silver');
+INSERT INTO ball (num, color) VALUES (2,'Blue');
+INSERT INTO ball (num, color) VALUES (2,'Yellow');
+INSERT INTO ball (num, color) VALUES (2,'Yellow Frog');
+INSERT INTO ball (num, color) VALUES (2,'Green 8');
